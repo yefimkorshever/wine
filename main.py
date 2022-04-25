c330212ce127ed1d78ef4b0009ae0668f45dde96
yk <yefim_korshever@mail.ru>
2022-04-25 17:06:26 +0300

    Add main()

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+def main():
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
+    
+    
+if __name__ == '__main__':
+    main()    
