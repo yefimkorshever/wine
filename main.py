c648b7960e58d8311533b2433cfc24438586eb70
yk <yefim_korshever@mail.ru>
2022-04-26 17:24:30 +0300

    Add loading wine cards from excel

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 import datetime
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+import pandas
 
 
 def foundation_date():
@@ -32,22 +33,29 @@ def agree_noun_with_number_ru(
         return agreed_number(number, plural_genitive)
 
 def main():
+    excel_data_df = pandas.read_excel(
+        'wine.xlsx',
+        sheet_name='Лист1',
+        dtype={'Цена': int, }
+    )
+    wines = excel_data_df.to_dict('records')
+       
     start_date = foundation_date()
     number_years = years_since_date(start_date)
     
     past_years = agree_noun_with_number_ru(number_years, 'год', 'года', 'лет')
-        
+    
     env = Environment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html', 'xml'])
     )
     
     template = env.get_template('template.html')
-    rendered_page = template.render(past_years=past_years)
-
+    rendered_page = template.render(past_years=past_years, wines=wines)
+    
     with open('index.html', 'w', encoding="utf8") as file:
         file.write(rendered_page)
-
+    
     server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
     server.serve_forever()
     
